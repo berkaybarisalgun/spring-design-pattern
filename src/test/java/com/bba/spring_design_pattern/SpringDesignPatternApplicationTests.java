f34c7b89596d298d6de143f96629a37f3898e080
Baris Algun <76767449+berkaybarisalgun@users.noreply.github.com>
2024-05-25 13:24:46 +0300

    project setup

---
+++ src/test/java/com/bba/spring_design_pattern/SpringDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bba.spring_design_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
